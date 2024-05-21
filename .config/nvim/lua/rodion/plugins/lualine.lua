c6c75699fb981eb3818976cfb47bf59e46795754
Rodion-Bozhenko <rodion.bozhenko@tutanota.com>
2024-05-21 11:12:52 +0200

    chore: neovim - change lualine theme

--- .config/nvim/lua/rodion/plugins/lualine.lua
+++ .config/nvim/lua/rodion/plugins/lualine.lua
@@ -3,19 +3,18 @@ if not status then
 	return
 end
 
-local theme = require("lualine.themes.nightfly")
+local theme = require("lualine.themes.rose-pine")
 
 local new_colors = {
 	blue = "#31748f",
 	green = "#9ccfd8",
-	violet = "#eb6f92",
 	yellow = "#f6c177",
 	black = "#191724",
 }
 
 theme.normal.a.bg = new_colors.blue
+theme.normal.b.fg = new_colors.blue
 theme.insert.a.bg = new_colors.green
-theme.visual.a.bg = new_colors.violet
 theme.command = {
 	a = {
 		gui = "bold",
@@ -27,5 +26,8 @@ theme.command = {
 lualine.setup({
 	options = {
 		theme = theme,
+	},
+	sections = {
+		lualine_c = { { "filename", path = 1 } },
 	},
 })
