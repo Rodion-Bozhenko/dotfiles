6c4250a3e94445931e82dec5bb93d2ba8827f9c1
Rodion-Bozhenko <rodion.bozhenko@tutanota.com>
2024-09-27 13:15:17 +0300

    fix: neovim - fix breaking changes in trouble 3.0

--- .config/nvim/lua/rodion/lazy-setup.lua
+++ .config/nvim/lua/rodion/lazy-setup.lua
@@ -230,8 +230,41 @@ require("lazy").setup({
 	},
 	{
 		"folke/trouble.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		cmd = { "Trouble", "TroubleRefresh", "TroubleClose", "TroubleToggle" },
+		opts = {},
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xl",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xq",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+		-- dependencies = { "nvim-tree/nvim-web-devicons" },
 	},
 	-- harpoon
 	{
