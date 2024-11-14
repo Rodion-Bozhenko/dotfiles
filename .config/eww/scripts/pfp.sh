50fb94c106ff7b7ae56ef1ba2c5d660f7abe6b24
Rodion-Bozhenko <rodion.bozhenko@tutanota.com>
2024-11-14 12:02:24 +0300

    fix: notification center - change avatar

--- .config/eww/scripts/pfp.sh
+++ .config/eww/scripts/pfp.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-declare -a profiles_photos=("$HOME/Pictures/shibainu.jpeg")
+declare -a profiles_photos=("$HOME/Pictures/shibainu.jpg")
 
 for pfp in ${profiles_photos[@]}; do
   if test -f $pfp; then
