a9cd162c34b3881e56e113759bb049d1fa52a0a4
Rodion-Bozhenko <rodion.bozhenko@tutanota.com>
2025-04-22 16:58:23 +0300

    feat(eww): remove notifications center

--- .config/eww/scripts/cpu.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-mpstat 1 1 | grep "Average" | awk '{usage=($3+$4+$5+$6+$7+$8+$9+$10+$11)} END {printf("%d\n",usage + 0.5)}'
--- .config/eww/scripts/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pkill eww
-pkill -f notifications.py
-~/bin/eww daemon
-~/bin/eww open --screen 0 notifications_popup
-~/bin/eww open --screen 0 control_center
-~/.config/eww/scripts/notifications.py &
