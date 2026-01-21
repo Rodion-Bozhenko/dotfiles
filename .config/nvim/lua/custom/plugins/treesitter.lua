1da43e0fce80ce876da72d41f823883da2f28c8f
Rodion-Bozhenko <rodion.bozhenko@tutanota.com>
2026-01-21 13:37:42 +0300

    feat(neovim): move to new tree-sitter version

--- .config/nvim/lua/custom/plugins/treesitter.lua
+++ .config/nvim/lua/custom/plugins/treesitter.lua
@@ -1,65 +1,107 @@
+-- This config taken from https://github.com/sharpchen/nix-config/blob/d357b8026930b9ec380e8cd480bbf983ed984cb0/dotfiles/nvim-config/lua/plugins/treesitter.lua#L55-L66
+---@diagnostic disable: missing-fields
+---@module 'lazy'
+---@type LazySpec
 return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    dependencies = {
-      "windwp/nvim-ts-autotag",
-      {
-        "nvim-treesitter/nvim-treesitter-context",
-        config = function()
-          local context = require("treesitter-context")
-          context.setup({
-            max_lines = 10,
-            multiline_threshold = 5,
-          })
-
-          vim.keymap.set("n", "[c", function()
-                           context.go_to_context()
-                         end, { silent = true })
-        end
-      },
-      "nvim-treesitter/nvim-treesitter-textobjects"
-    },
-    event = { "BufReadPre", "BufNewFile" },
+    branch = "main",
     config = function()
-      require "nvim-treesitter.configs".setup {
-        ensure_installed = {
-          "c", "css", "dockerfile", "go", "gomod", "html", "hyprlang", "javascript", "json",
-          "just", "rust", "sql", "templ", "terraform", "toml", "typescript", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline"
-        },
-        sync_install = false,
-        auto_install = false,
-        indent = { enable = true },
-        autotag = { enable = true },
-        highlight = {
+      local treesitter = require("nvim-treesitter")
+      treesitter.setup {}
+      local should_install = {
+        "c", "css", "dockerfile", "go", "gomod", "html", "hyprlang", "javascript",
+        "json", "tsx",
+        "just", "rust", "sql", "templ", "terraform", "toml", "typescript", "lua",
+        "vim", "vimdoc", "query", "markdown", "markdown_inline"
+      }
+
+      local result = {}
+      local seenInResult = {}
+      local lookupSub = {}
+
+      for _, value in ipairs(treesitter.get_installed()) do
+        lookupSub[value] = true
+      end
+
+      for _, value in ipairs(should_install) do
+        if not lookupSub[value] and not seenInResult[value] then
+          table.insert(result, value)
+          seenInResult[value] = true
+        end
+      end
+
+      treesitter.install(result)
+
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(args)
+          if
+              vim.list_contains(
+                treesitter.get_installed(),
+                vim.treesitter.language.get_lang(args.match)
+              )
+          then
+            vim.treesitter.start(args.buf)
+          end
+        end,
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("nvim-treesitter-textobjects").setup {
+        select = {
           enable = true,
-          disable = function(lang, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat,
-                                    vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-              return true
-            end
-          end,
-          additional_vim_regex_highlighting = false,
-        },
-        textobjects = {
-          select = {
-            enable = true,
-            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-            keymaps = {
-              ["aa"] = "@parameter.outer",
-              ["ia"] = "@parameter.inner",
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-              ["ai"] = "@conditional.outer",
-              ["ii"] = "@conditional.inner",
-            },
+          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+          keymaps = {
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+            ["ai"] = "@conditional.outer",
+            ["ii"] = "@conditional.inner",
           },
+          include_surrounding_whitespace = true,
         },
       }
+      do -- move
+        vim.keymap.set({ "n", "x", "o" }, "]]", function()
+          require("nvim-treesitter-textobjects.move").goto_next_start(
+            "@function.outer",
+            "textobjects"
+          )
+          vim.cmd("normal! zz")
+        end)
+        vim.keymap.set({ "n", "x", "o" }, "[[", function()
+          require("nvim-treesitter-textobjects.move").goto_previous_start(
+            "@function.outer",
+            "textobjects"
+          )
+          vim.cmd("normal! zz")
+        end)
+      end
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    config = function()
+      local context = require("treesitter-context")
+      context.setup({
+        max_lines = 10,
+        multiline_threshold = 5,
+      })
+
+      vim.keymap.set("n", "[c", function()
+                       context.go_to_context()
+                     end, { silent = true })
     end
   },
 }
