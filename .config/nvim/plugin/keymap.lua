74fe8e937803df0223ead9af44cc17feb6adfeb5
Rodion-Bozhenko <rodion.bozhenko@tutanota.com>
2026-01-21 13:42:35 +0300

    feat(neovim): add harpoon

--- .config/nvim/plugin/keymap.lua
+++ .config/nvim/plugin/keymap.lua
@@ -46,6 +46,29 @@ keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- oil.nvim
 keymap.set("n", "<leader>e", "<cmd>Oil<CR>")
 
+local harpoon = require("harpoon")
+
+-- REQUIRED
+harpoon:setup()
+-- REQUIRED
+
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+vim.keymap.set("n", "<C-e>",
+               function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+
+-- Leap
+vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)")
+vim.keymap.set("n", "S", "<Plug>(leap-from-window)")
+
 -- keymap.set("n", "]d",
 --            function()
 --              vim.diagnostic.jump({
